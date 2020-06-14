510240048c629728ded691ab0bad2472cf7ca5d3
James Quick <james.q.quick@outlook.com>
2020-06-13 19:10:38 -0500

    Added more links

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 console.log(`James Quick
 Website - https://www.jamesqquick.com
+Courses - https://www.jamesqquick.com/courses
 Twitter - https://www.twitter.com/jamesqquick
+YouTube - https://www.youtube.com/jamesqquick
+Twitch - https://www.twitch.tv/jamesqquick
+Github - https://www.github.com/jamesqquick
+LinkedIn: https://linkedin.com/in/jamesqquick   
 Email - me@jamesqquick.com`);
